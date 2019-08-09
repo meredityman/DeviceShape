6e58d7bd079bce578764dc6baee8c1ea409ad315
Meredith <meredityman@gmail.com>
2019-08-09 12:57:31 +0200

    Test commit

--- src/Power.py
+++ src/Power.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 
 class Power():
-    LOW_VOLTAGE_PIN = 16
+    LOW_VOLTAGE_GPIO = 16
     def __init__(self) :
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.LOW_VOLTAGE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.LOW_VOLTAGE_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         
         self.is_low_power()
     
@@ -12,7 +12,7 @@ class Power():
         return [("/power/low/", self.is_low_power())]    
     
     def is_low_power(self):
-        self.low_power = GPIO.input(self.LOW_VOLTAGE_PIN)
+        self.low_power = GPIO.input(self.LOW_VOLTAGE_GPIO)
         return self.low_power
     
     
