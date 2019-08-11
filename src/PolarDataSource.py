858698f7b75d9ab2867f21b17e7629d2878867bc
Meredith <meredityman@gmail.com>
2019-08-11 23:25:45 +0200

    WOrking acc

--- src/PolarDataSource.py
+++ src/PolarDataSource.py
@@ -9,6 +9,7 @@ UUID_CHARACTER_HR_MEASURE   = '00002a37-0000-1000-8000-00805f9b34fb'
 UUID_PWM_CONTROL_POINT      = 'fb005c81-02e7-f387-1cad-8acd2d8df0c8'
 UUID_PWM_DATA               = 'fb005c82-02e7-f387-1cad-8acd2d8df0c8'
 
+
 class PolarDataSource(BaseDataSource):
 
     def __init__(self, mac_address):
@@ -62,16 +63,20 @@ class PolarDataSource(BaseDataSource):
         self.connecting = False
 
 
-    def disconnect_handler(self):
+    def disconnect_handler(self, client):
         print("Polar ({}): disconnected".format(self.mac_address))
         self.connected = False
 
     async def start_notify(self):
-            #await self.client.start_notify(UUID_PWN_DATA            , self.data_handler)
+            
+            await self.client.start_notify(UUID_PWM_DATA            , self.data_handler)
 
-            await self.client.write_gatt_char(UUID_PWM_CONTROL_POINT, bytearray(b'\x01\x02'), response = True)
+            await self.client.write_gatt_char(UUID_PWM_CONTROL_POINT, bytearray([0x02, 0x02, 0x00, 0x01, 0x19, 0x00, 0x01, 0x01, 0x10, 0x00, 0x02, 0x01, 0x08, 0x00]), response=True)
+
 
             await self.client.start_notify(UUID_CHARACTER_HR_MEASURE, self.hr_handler)
+            
+
             #await self.client.set_disconnected_callback(self.disconnect_handler)
 
             self.is_setup = True
@@ -98,15 +103,27 @@ class PolarDataSource(BaseDataSource):
         ]
         
     def hr_handler(self, sender, data):
-        print("HR: {}".format(int(data[1])))
+        #print("HR: {}".format(int(data[1])))
         self.hr_latest = int(data[1])
         self.data.append({ "HR" : (datetime.now(), self.hr_latest  )})
 
     def data_handler(self, sender, data):
-        print("hey")
-        #print("Data: {}".format(data))
+        if(data[0] == 2):
+            frames = data[10:]
+            
+            dat = []
+            for i in range(0, len(frames), 6):
+                x = int.from_bytes(frames[i:i+2]  , byteorder='little', signed=True)
+                y = int.from_bytes(frames[i+2:i+4], byteorder='little', signed=True)
+                z = int.from_bytes(frames[i+4:i+6], byteorder='little', signed=True)
+                print("{}, {}, {}".format(x, y, z))
+                dat.append(x)
+                dat.append(y)
+                dat.append(z)
 
-    
+            self.data.append({"ACC" : (datetime.now(), dat) })
+
+
     async def printServices(self, client):
         for service in client.services:
             print("[Service] {0}: {1}".format(service.uuid, service.description))
